170a4a4145f52f3c019c9cbe08451cbb99b1486b
임한결 <dlagksruf19@naver.com>
2021-03-07 17:10:54 +0900

    [baekjoon] DFS와 BFS 재풀이 (DFS, BFS)

--- codingtest/baekjoon/DfaAndBfs.java
+++ codingtest/baekjoon/DfaAndBfs.java
@@ -6,71 +6,69 @@ import java.util.*;
  */
 class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] firstInputs = bufferedReader.readLine().split(" ");
-
-        int node = toInt(firstInputs[0]);
-        int edge = toInt(firstInputs[1]);
-        int startNode = toInt(firstInputs[2]);
+        int node = toInt(bufferedReader.readLine());
+        int edge = toInt(bufferedReader.readLine());
 
         int[][] matrix = new int[node + 1][node + 1];
+        boolean[] visited = new boolean[node + 1];
 
         for (int i = 0; i < edge; i++) {
-            String[] secondInputs = bufferedReader.readLine().split(" ");
-            int x = toInt(secondInputs[0]);
-            int y = toInt(secondInputs[1]);
+            String[] inputEdge = bufferedReader.readLine().split(" ");
+            int x = toInt(inputEdge[0]);
+            int y = toInt(inputEdge[1]);
 
             matrix[x][y] = matrix[y][x] = 1;
         }
 
-        boolean[] visited = new boolean[node + 1];
-        dfs(matrix, visited, startNode);
-
+        int count = dfs(1, matrix, visited, 0);
+        System.out.println(count);
         System.out.println();
 
-        visited = new boolean[node + 1];
-        bfs(matrix, visited, startNode);
+       visited = new boolean[node + 1];
+
+       bfs(matrix, visited, startNode);
     }
 
-    private static int toInt(String value) {
-        return Integer.parseInt(value);
-    }
+    private static int dfs(int startNode, int[][] matrix, boolean[] visited, int count) {
+        visited[startNode] = true;
 
-    private static void dfs(int[][] matrix, boolean[] visited, int startNode) {
-        int matrixLength = matrix.length;
-        if (startNode == matrixLength) {
-            return;
+        if (startNode == matrix.length) {
+            return 0;
         }
 
-        visited[startNode] = true;
-        System.out.print(startNode + " ");
-
-        for (int i = 1; i < matrixLength; i++) {
+        for (int i = 1; i < matrix.length - 1; i++) {
             if (matrix[startNode][i] == 1 && !visited[i]) {
-                dfs(matrix, visited, i);
+                count++;
+                dfs(i, matrix, visited, count);
             }
         }
+        return count;
     }
 
     private static void bfs(int[][] matrix, boolean[] visited, int startNode) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(startNode);
         visited[startNode] = true;
-        System.out.print(startNode + " ");
 
         while (!queue.isEmpty()) {
             int node = queue.poll();
+            System.out.print(node + " ");
 
             for (int i = 1; i < matrix.length; i++) {
                 if (matrix[node][i] == 1 && !visited[i]) {
                     queue.offer(i);
                     visited[i] = true;
-                    System.out.print(i + " ");
                 }
             }
+
         }
+    }
+
+    private static int toInt(String value) {
+        return Integer.parseInt(value);
     }
 
 }

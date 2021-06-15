4f8d1c104901d53e2440d9104a4c8f97162107c5
limhangyeol <dlagksruf19@naver.com>
2021-06-15 13:05:39 +0900

    [programmers] 우유와 요거트가 담긴 장바구니 (sql - level4)

---
+++ src/main/resources/sql/Cart.sql
@@ -0,0 +1,11 @@
+-- Programmers
+-- SQL - Level4
+-- 우유과 요거트가 담긴 장바구니
+SELECT
+    DISTINCT C1.CART_ID
+FROM
+    CART_PRODUCTS C1
+        JOIN CART_PRODUCTS C2
+             ON C1.CART_ID = C2.CART_ID
+WHERE
+        C1.NAME = "Milk" AND C2.NAME = 'Yogurt';
